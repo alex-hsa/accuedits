526be3605a400a8844aea9585a949c5b7e3c6ef0
Alex Ismail <alexi721@gmail.com>
2024-05-28 12:31:55 -0700

    Upgraded Content Templates module and exported config

--- web/modules/contrib/content_templates/src/ContentTemplateListBuilder.php
+++ web/modules/contrib/content_templates/src/ContentTemplateListBuilder.php
@@ -5,6 +5,7 @@ namespace Drupal\content_templates;
 use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Entity\EntityListBuilder;
 use Drupal\Core\Link;
+use Drupal\Core\Url;
 
 /**
  * Defines a class to build a listing of content template entities.
@@ -19,6 +20,7 @@ class ContentTemplateListBuilder extends EntityListBuilder {
   public function buildHeader() {
     $header['name'] = $this->t('Name');
     $header['category'] = $this->t('Category');
+    $header['status'] = $this->t('Status');
     return $header + parent::buildHeader();
   }
 
@@ -33,6 +35,7 @@ class ContentTemplateListBuilder extends EntityListBuilder {
       ['node' => $entity->get('field_source')->target_id]
     );
     $row['category'] = $entity->get('field_category')->entity ? $entity->get('field_category')->entity->label() : $this->t('Uncategorized');
+    $row['status'] = $entity->isPublished() ? $this->t('Active') : $this->t('Disabled');
     return $row + parent::buildRow($entity);
   }
 
@@ -41,7 +44,18 @@ class ContentTemplateListBuilder extends EntityListBuilder {
    */
   public function getOperations(EntityInterface $entity) {
     $node = $entity->get('field_source')->entity;
-    return parent::getOperations($node);
+    $operations = parent::getOperations($node);
+    $operations['view'] = [
+      'title' => $this->t('View'),
+      'weight' => -10,
+      'url' => Url::fromRoute('entity.node.canonical', ['node' => $node->id()]),
+    ];
+    $operations['descendants'] = [
+      'title' => $this->t('Descendants'),
+      'weight' => 101,
+      'url' => Url::fromRoute('content_templates.node.overview', ['node' => $node->id()]),
+    ];
+    return $operations;
   }
 
 }
